62a3b91222ea57a79882a44950ec50ee1ab8040a
Ven Ten <aminuisrael2@gmail.com>
2020-03-05 03:18:04 +0100

    solved static file issue

--- model_deploy/settings.py
+++ model_deploy/settings.py
@@ -119,11 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 django_heroku.settings(locals())
